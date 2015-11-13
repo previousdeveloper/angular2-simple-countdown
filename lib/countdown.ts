60d989999fce29935da033b7f71d65934c8eafea
previousdeveloper <gokhan.karadas1992@gmail.com>
2015-11-14 01:04:45 +0200

    init project

---
+++ lib/countdown.ts
@@ -0,0 +1,74 @@
+import {Component,FORM_DIRECTIVES} from 'angular2/angular2';
+
+
+@Component({
+    selector: 'count-down',
+    properties: [
+        'units',
+        'end'
+    ],
+    directives: [FORM_DIRECTIVES],
+    template: `<h1>{{displayString()}}</h1>
+  <ng-content></ng-content>
+  `
+})
+
+
+export class CountDown {
+    units:any;
+    end:any;
+
+    displayString() {
+
+        if (typeof this.units === 'string') {
+            this.units = this.units.split('|');
+        }
+
+
+        var dateDifference = new Date(this.end) - new Date();
+        var lastUnit = this.units[this.units.length - 1],
+            unitConstantForMillisecs = {
+                weeks: (1000 * 60 * 60 * 24 * 7),
+                days: (1000 * 60 * 60 * 24),
+                hours: (1000 * 60 * 60),
+                minutes: (1000 * 60),
+                seconds: 1000,
+                milliseconds: 1
+            },
+            unitsLeft = {},
+            returnString = '',
+            totalMillisecsLeft = dateDifference,
+            i,
+            unit:any;
+        for (i in this.units) {
+            if (this.units.hasOwnProperty(i)) {
+
+                unit = this.units[i].trim();
+                if (unitConstantForMillisecs[unit.toLowerCase()] === false) {
+                    //$interval.cancel(countDownInterval);
+                    throw new Error('Cannot repeat unit: ' + unit);
+
+                }
+                if (unitConstantForMillisecs.hasOwnProperty(unit.toLowerCase()) === false) {
+                    throw new Error('Unit: ' + unit + ' is not supported. Please use following units: weeks, days, hours, minutes, seconds, milliseconds');
+                }
+
+                unitsLeft[unit] = totalMillisecsLeft / unitConstantForMillisecs[unit.toLowerCase()];
+
+                if (lastUnit === unit) {
+                    unitsLeft[unit] = Math.ceil(unitsLeft[unit]);
+                } else {
+                    unitsLeft[unit] = Math.floor(unitsLeft[unit]);
+                }
+                totalMillisecsLeft -= unitsLeft[unit] * unitConstantForMillisecs[unit.toLowerCase()];
+                unitConstantForMillisecs[unit.toLowerCase()] = false;
+
+
+                returnString += ' ' + unitsLeft[unit] + ' ' + unit;
+            }
+        }
+        return returnString;
+    }
+
+
+}
